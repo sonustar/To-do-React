8ba00687389bfe65435f822239e002033e442c6f
sonustar <91054530+sonustar@users.noreply.github.com>
2024-01-12 15:09:10 +0000

    Navbar created

--- Exam-helper/src/Components/Navbar.jsx
+++ Exam-helper/src/Components/Navbar.jsx
@@ -1,13 +1,17 @@
-
+import { FaComment } from "react-icons/fa";
+import "./Navbar.css";
 import React from 'react'
 import { CiBellOn } from "react-icons/ci";
+import { IoIosContact } from "react-icons/io";
 
 const Navbar = () =>{
 
   return (
    
-    <div>
-      <img src="" alt="" />
+    <div className="navbar">
+      
+      <FaComment />
+      {/* <img src="" alt="" /> */}
           
           <ul>
             <li>Home</li>
@@ -15,14 +19,17 @@ const Navbar = () =>{
             <li>FAQ</li>
             <li>About Us </li>
             <li>Contact Us </li>
+            {/* do */}
             
           </ul>
          
       <div className='search-box'>
-        <input type="text" placeholder='search' />
-        <CiBellOn />
+        <input type="text" placeholder='search for exams' />
+        <h1> <CiBellOn className="icon"/> </h1> 
+       {/* CiBel <lOn className="icon"/>/ */}
       </div>
-
+    
+      <h1>< IoIosContact /></h1> 
     </div>
 
   );
